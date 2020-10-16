89128d0061a9ab8f2d829e03073cc7c1a84ce680
dylan <dcjohnson24@gmail.com>
2020-10-16 18:10:57 -0500

    Add arrow edges, hover text, and text annotations

--- model/visualize.py
+++ model/visualize.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
 import plotly.graph_objects as go
-from d3graph import d3graph, vec2adjmat
+
+from addEdge import addEdge
 
 
 def remove_frozensets(rules: pd.DataFrame) -> pd.DataFrame:
@@ -82,16 +83,18 @@ def plot_heatmap_seaborn(rules: pd.DataFrame, plot_val: str) -> None:
     plt.show()
 
 
-def plot_network_graph_d3(rules: pd.DataFrame, weight_var: str) -> None:
-    rules = remove_frozensets(rules)
-    adjmat = vec2adjmat(source=rules['antecedents_'],
-                        target=rules['consequents_'])
-    node_size = rules[f'{weight_var}'].tolist()
-    d3graph(adjmat, node_size_edge=node_size, directed=True)
+def plot_network_graph_plotly(rules: pd.DataFrame, weight_var: str) -> None:
+    """ Create a network of items and rules where edges contain
+    various scores such as confidence, lift, etc.
 
-
-def plot_network_graph(rules: pd.DataFrame, weight_var: str) -> None:
+    Args:
+        rules (pd.DataFrame): DataFrame of association rules from mlxtend
+        weight_var (str): the association metric to use. Must be one of 
+            'confidence', 'lift', 'leverage', or 'conviction'.
+    """
     rules = remove_frozensets(rules)
+    rules[f'{weight_var}'] = rules[f'{weight_var}'].round(2)
+
     G1 = nx.from_pandas_edgelist(
         rules,
         source='antecedents_',
@@ -103,26 +106,34 @@ def plot_network_graph(rules: pd.DataFrame, weight_var: str) -> None:
     for n, p in pos.items():
         G1.nodes[n]['pos'] = p
 
-    edge_trace = go.Scatter(
-        x=[],
-        y=[],
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines'
-    )
-
     xmids = []
     ymids = []
+    edge_x = []
+    edge_y = []
 
     for edge in G1.edges():
         x0, y0 = G1.nodes[edge[0]]['pos']
         x1, y1 = G1.nodes[edge[1]]['pos']
         xmids.append((x0 + x1)/2)
         ymids.append((y0 + y1)/2)
-        edge_trace['x'] += tuple([x0, x1, None])
-        edge_trace['y'] += tuple([y0, y1, None])
+        edge_x, edge_y = addEdge(
+            start=(x0, y0),
+            end=(x1, y1),
+            edge_x=edge_x,
+            edge_y=edge_y,
+            arrowPos='end',
+            arrowLength=0.04
+        )
 
-    etext = [f'weight: {w}' for w in
+    edge_trace = go.Scatter(
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=1.5, color='#888'),
+        hoverinfo='none',
+        mode='lines'
+    )
+
+    etext = [f'{weight_var}: {w}' for w in
              list(nx.get_edge_attributes(G1, f'{weight_var}').values())]
 
     eweights_trace = go.Scatter(
@@ -141,7 +152,7 @@ def plot_network_graph(rules: pd.DataFrame, weight_var: str) -> None:
         mode='markers',
         hoverinfo='text',
         marker_size=15,
-        marker_color='RoyalBlue'
+        marker_color='#000080'
     )
 
     for node in G1.nodes():
@@ -152,54 +163,21 @@ def plot_network_graph(rules: pd.DataFrame, weight_var: str) -> None:
     fig = go.Figure(
         data=[edge_trace, node_trace, eweights_trace],
         layout=go.Layout(
-            title='<br>Retail Items Network Connections',
+            title=f'<b>Retail Items Network Connections'
+                  f' based on {weight_var} score </b>',
             titlefont=dict(size=16),
+            title_x=0.5,
             showlegend=False,
             hovermode='closest',
             margin=dict(b=20, l=5, r=5, t=40),
             xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+        )
+    )
+    fig.add_annotation(
+        text=f'<b>Note:</b> Hover over node to get name of item<br>'
+             f' Hover over edge arrow between'
+             f' nodes to get {weight_var} score<br>'
+             f' Arrow represents rule A &#8594; C'
     )
     fig.show()
-
-
-def draw_graph(rules: pd.DataFrame, rules_to_show: int) -> None:
-    G1 = nx.DiGraph()
-    color_map = []
-    N = 50
-    colors = np.random.rand(N)
-    strs = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5',
-            'R6', 'R7', 'R8', 'R9', 'R10', 'R11']
-
-    for i in range(rules_to_show):
-        G1.add_nodes_from(["R"+str(i)])
-        for a in rules.iloc[i]['antecedents']:
-            G1.add_nodes_from([a])
-            G1.add_edge(a, "R"+str(i), color=colors[i], weight=2)
-        for c in rules.iloc[i]['consequents']:
-            G1.add_nodes_from([c])
-            G1.add_edge("R"+str(i), c, color=colors[i],  weight=2)
-
-    for node in G1:
-        found_a_string = False
-        for item in strs:
-            if node == item:
-                found_a_string = True
-        if found_a_string:
-            color_map.append('yellow')
-        else:
-            color_map.append('green')
-
-    edges = G1.edges()
-    colors = [G1[u][v]['color'] for u, v in edges]
-    weights = [G1[u][v]['weight'] for u, v in edges]
-
-    pos = nx.spring_layout(G1, k=16, scale=1)
-    nx.draw(G1, pos, node_color=color_map,
-            edge_color=colors, width=weights, font_size=16,
-            with_labels=False)
-
-    for p in pos:  # raise text positions
-        pos[p][1] += 0.07
-    nx.draw_networkx_labels(G1, pos)
-    plt.show()
