3ae2c18530cea057081c127c4f9e7b7c5b59cb73
dylan <dcjohnson@gmail.com>
2022-01-20 21:30:08 -0600

    Update docker installation steps

--- setup_server.sh
+++ setup_server.sh
@@ -8,30 +8,43 @@
 # make a directory for the celery logs /var/log/supervisor/
 # change permissions on /var/log/supervisor directory to 777
 LIST_OF_APPS="virtualenv supervisor nginx redis-server"
-echo "Installing packages $LIST_OF_APPS"
+echo "--> Installing packages $LIST_OF_APPS"
 sudo apt-get update -y && sudo apt-get install -y $LIST_OF_APPS
 
-echo "Copying nginx and supervisor configs"
+echo "--> Copying nginx and supervisor configs"
 sudo cp deployment/nginx/market_basket_app /etc/nginx/sites-available/
 sudo cp deployment/supervisor/configs/conf.d/* /etc/supervisor/conf.d/
 
-echo "Creating log folder for supervisor"
+echo "--> Creating log folder for supervisor"
 sudo mkdir -p /var/log/supervisor
 sudo chmod 777 /var/log/supervisor
 
-echo "Creating log folder for celery"
+echo "-- > Creating log folder for celery"
 sudo mkdir -p /var/log/celery
 sudo chmod 777 /var/log/celery
 
-echo "Installing docker"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-cache policy docker-ce
-sudo apt install docker-ce
+echo "--> Installing docker"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-echo "Check that docker is running"
+curl \
+    -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "--> Check that docker is running"
 sudo systemctl status docker
 
-echo "Run docker without sudo"
-sudo usermod -aG docker ${USER}
-su - ${USER}
+echo "--> Run docker without sudo"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
